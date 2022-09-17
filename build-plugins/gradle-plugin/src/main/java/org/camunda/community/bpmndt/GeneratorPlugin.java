ae9912ce2749f26d7d6798d65e0a1c8258868bbf
Thomas Peetz <thomas.peetz@thpeetz.de>
2022-09-18 01:47:15 +0200

    Setup Gradle multimodule build
    Create Gradle multimodule build build-plugins with subdirectories for
    Gradle and Maven plugin.
    Setup Build for Gradle plugin wit empty definition for plugin.

---
+++ build-plugins/gradle-plugin/src/main/java/org/camunda/community/bpmndt/GeneratorPlugin.java
@@ -0,0 +1,14 @@
+package org.camunda.community.bpmndt;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GeneratorPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ build-plugins/gradle-plugin/src/test/groovy/org.camunda.community.bpmndt/GeneratorPluginTest.groovy
@@ -0,0 +1,42 @@
+package org.camunda.community.bpmndt
+
+import org.gradle.testkit.runner.GradleRunner
+import spock.lang.Specification
+import spock.lang.TempDir
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class GeneratorPluginTest extends Specification {
+    @TempDir
+    File testProjectDir
+    File buildFile
+    
+    def setup() {
+        buildFile = new File(testProjectDir, 'build.gradle')
+        buildFile << """
+            plugins {
+                id 'org.camunda.community.bpmndt'
+            }
+        """
+    }
+    
+
+    def "can successfully configure URL through extension and verify it"() {
+        buildFile << """
+            verification {
+                url = 'https://www.google.com/'
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments('verifyUrl')
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.output.contains("Successfully resolved URL 'https://www.google.com/'")
+        result.task(":verifyUrl").outcome == SUCCESS
+    }
+}
