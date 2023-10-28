e030f1be3f3eb3d83ea62a6bb604e059b815df17
Gabor Claussnitzer <gabor.claussnitzer@googlemail.com>
2023-10-28 09:33:01 +0200

    feat: added additional verifier for external tasks and their local variables

---
+++ integration-tests/advanced/src/test/java/org/example/it/ExternalTaskInputOutputTest.java
@@ -0,0 +1,34 @@
+package org.example.it;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import generated.externaltaskinputoutput.TC_startEvent__endEvent;
+
+public class ExternalTaskInputOutputTest {
+
+  @Rule
+  public TC_startEvent__endEvent tc = new TC_startEvent__endEvent();
+
+  @Test
+  public void testExecute() {
+    tc.handleExternalTask().verifyTask((externalTaskAssert, localVariables) -> {
+      externalTaskAssert.hasTopicName("test-topic");
+
+      assertThat(localVariables.get("increment"), is(11L));
+    });
+
+    tc.createExecutor()
+        .withVariable("i", 10L)
+        .verify(pi -> {
+          pi.isEnded();
+
+          // verify output variables
+          pi.variables().containsEntry("decrement", 10L);
+        })
+        .execute();
+  }
+}
